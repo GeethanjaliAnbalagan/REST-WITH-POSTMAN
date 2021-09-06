ea9019f6705aa3ba5487435108ef9ca1379f7b7f
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2021-09-06 12:28:24 +0530

    Add files via upload

---
+++ spring-boot-RESTMETHODS-1/src/test/java/com/java/springbootRESTMETHODS/SpringBootRestmethods1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springbootRESTMETHODS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestmethods1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
